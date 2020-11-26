903f564c458434f9a1cc8424df3590ab9635368e
Selene180 <hiroshi012042@gmail.com>
2020-11-26 20:37:41 +0900

    商品詳細表示機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_tweet, only: [:edit, :show]
 
   def index
     @items = Item.order('created_at DESC')
@@ -19,7 +20,20 @@ class ItemsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  private
+
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :status_id, :delivery_fee_id, :prefecture_id, :delivery_time_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_tweet
+    @item = Item.find(params[:id])
   end
 end
