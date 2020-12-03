cde811ce89c3b72b83ebb53145da46a30e0c0713
Selene180 <hiroshi012042@gmail.com>
2020-12-03 17:50:15 +0900

    商品購入機能の実装

---
+++ app/controllers/purchase_records_controller.rb
@@ -0,0 +1,33 @@
+class PurchaseRecordsController < ApplicationController
+  def index
+    @item = Item.find(params[:item_id])
+    @purchase_address = PurchaseAddress.new
+  end
+
+  def create
+    @item = Item.find(params[:item_id])
+    @purchase_address = PurchaseAddress.new(purchase_record_params)
+    if @purchase_address.valid?
+      pay_item
+      @purchase_address.save
+      redirect_to root_path
+    else
+      render 'index'
+    end
+  end
+
+  private
+
+  def purchase_record_params
+    params.require(:purchase_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :purchase_record).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: purchase_record_params[:token],
+      currency: 'jpy'
+    )
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
+  resources :users, only: [:create, :index]
+  resources :purchase_address, only:[:create]
+  
   resources :items, only: [:new, :create, :show, :edit, :update, :destroy] do
-    resources :users, only: [:create, :index]
+    resources :purchase_records, only:[:index, :create]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
